0753f3ba3eec65a1b607acf92a698cd4d0f04728
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-08-29 13:45:10 -0300

    fix: fixing seller page

--- react/SellerStars.js
+++ react/SellerStars.js
@@ -1,62 +1,69 @@
-import React, {useState} from "react"
-import { Link } from 'vtex.render-runtime'
-import { useProduct } from 'vtex.product-context'
-import { useCurrentSeller } from 'vtex.seller-selector/CurrentSellerContext'
+import React, { useState } from "react";
+import { Link } from "vtex.render-runtime";
+import { useProduct } from "vtex.product-context";
+import { useCurrentSeller } from "vtex.seller-selector/CurrentSellerContext";
 
-const SellerStars = ({Stars, SellerInfos}) => {
+const SellerStars = ({ Stars, SellerInfos }) => {
+  const { product, selectedItem } = useProduct() ?? {};
+  const { currentSeller } = useCurrentSeller();
 
-  const { product, selectedItem } = useProduct() ?? {}
-  const { currentSeller } = useCurrentSeller()
-
-  const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <div style={{cursor: "pointer"}} onClick={() => {setShowModal(true)}}>
+      <div
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
         <span>{currentSeller.sellerName}</span>
         <Stars sellerId={currentSeller.sellerId} />
       </div>
-      {
-          showModal && (
-            <div style={{
-              display: "flex",
-              flexDirection:"column",
-              alignItems:"center",
+      {showModal && (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            position: "absolute",
+            left: "56vw",
+            width: "fit-content",
+            padding: "10px",
+            height: "130px",
+            backgroundColor: "white",
+            border: "1px solid #000",
+            borderRadius: "5px",
+            zIndex: 99,
+          }}
+        >
+          <span
+            style={{
+              cursor: "pointer",
               position: "absolute",
-              left: "56vw",
-              width: "fit-content",
-              padding: "10px",
-              height: "130px",
-              backgroundColor: "white",
-              border: "1px solid #000",
-              borderRadius: "5px",
-              zIndex: 99
-            }} >
-              <span 
-              style={{
-                cursor: "pointer",
-                position: "absolute",
-                right: "5px",
-                top: "5px",
-              }} 
-              onClick={() => {setShowModal(false)}}
-              >X</span>
+              right: "5px",
+              top: "5px",
+            }}
+            onClick={() => {
+              setShowModal(false);
+            }}
+          >
+            X
+          </span>
 
-              <SellerInfos sellerId={currentSeller.sellerId} />
+          <SellerInfos sellerId={currentSeller.sellerId} />
 
-              <Link
-                className={`seller-click`}
-                page="store.seller"
-                params={{slug: product?.linkText}}
-                query={currentSeller ? `sellerId=${currentSeller.sellerId}` : ''}
-              >
-                Ver perfil do lojista
-              </Link>
-            </div>
-          )
-        }
+          <Link
+            className={`seller-click`}
+            page="store.custom#seller"
+            query={currentSeller ? `sellerId=${currentSeller.sellerId}` : ""}
+          >
+            Ver perfil do lojista
+          </Link>
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default SellerStars
+export default SellerStars;
