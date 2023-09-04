2d9eace230b8e4054a591877477d75f125ab8798
Luis F K Andriolo <luisfandriolo@gmail.com>
2023-09-04 19:13:04 -0300

    Fix: fixing sellerId

--- react/SellerPage.js
+++ react/SellerPage.js
@@ -1,35 +1,36 @@
-import React, { useContext, useEffect, useRef, useState } from "react"
-import { useProduct } from 'vtex.product-context'
-import { useRuntime } from 'vtex.render-runtime'
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useProduct } from "vtex.product-context";
+import { useRuntime } from "vtex.render-runtime";
 
-const SellerPage = ({children, Products}) => {
-
-  const { selectedItem } = useProduct()
-  const { query } = useRuntime()
-
-  const sellers = selectedItem?.sellers
-
-  const actualSeller = sellers?.find((s) => s.sellerId === query.sellerId)
+const SellerPage = ({ children, Products }) => {
+  const { query } = useRuntime();
+  const actualSeller = query.sellerId;
 
   return (
     <>
-      <div style={{
+      <div
+        style={{
           display: "flex",
-          flexDirection:"column",
+          flexDirection: "column",
           justifyContent: "center",
-          alignItems: "center"
-        }}>
+          alignItems: "center",
+        }}
+      >
         <h2>{actualSeller?.sellerName}</h2>
         {children}
       </div>
       <div>
-        <h2 style={{
-          textAlign: "center"
-        }}>Produtos</h2>
+        <h2
+          style={{
+            textAlign: "center",
+          }}
+        >
+          Produtos
+        </h2>
         {Products}
       </div>
     </>
-  )
-}
+  );
+};
 
-export default SellerPage
+export default SellerPage;
--- react/SellerTestimonials.js
+++ react/SellerTestimonials.js
@@ -3,12 +3,8 @@ import { useProduct } from "vtex.product-context";
 import { useRuntime } from "vtex.render-runtime";
 
 const SellerTestimonials = ({ Testimonials }) => {
-  const { selectedItem } = useProduct();
   const { query } = useRuntime();
-
-  const sellers = selectedItem?.sellers;
-
-  const actualSeller = sellers?.find((s) => s.sellerId === query.sellerId);
+  const actualSeller = query.sellerId;
 
   return (
     <div>
